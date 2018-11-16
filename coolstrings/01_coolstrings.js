e4182b6936d85866bc97dd7b840c60f2587399d6
watsbeat <c.watsonbeaton@gmail.com>
2018-11-16 23:29:06 +1000

    solved morning challenge

--- coolstrings/01_coolstrings.js
+++ coolstrings/01_coolstrings.js
@@ -1,20 +1,26 @@
-// Create a new variable called coolstrings which cannot be reassigned
-// and set its value to 'coolstrings'
+/* Unit: Ruby vs JS
 
-// Create a second variable called numbers which can be reassigned
-// and set its value to 7
+COOL STRINGS
+Morning Challenge 1
+--------------------
+1. Create a new variable called coolstrings which cannot be reassigned
+2. Set its value to 'coolstrings'
+3. Create a second variable called numbers which can be reassigned
+4. Set its value to 7
 
-// Instead of using:
-  // def()
-  // 
-  // end
-// What is the main way to write methods in JavaScript?
+Questions
+--------------------
+Instead of using:
+  def()
+  //
+  end
+1. What is the main way to write methods in JavaScript? function functionName() {}
+2. Instead of puts, what does JavaScript use instead? console.log()
 
-// Instead of puts, what does JavaScript use instead?
+*/
 
-
-
-
+const coolstrings = 'coolstrings' // cannot be reassigned
+var numbers = 7 // can be reassigned
 
 function setNumbers(num) {
   numbers = num;  
