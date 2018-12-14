08d86ed422be0d75beb247fb8b5744dd3458c5a6
watsbeat <c.watsonbeaton@gmail.com>
2018-12-14 21:55:22 +1000

    solved

--- 09_odd_or_even.js
+++ 09_odd_or_even.js
@@ -7,36 +7,22 @@ Give your answer in string format as 'odd' or 'even'.
 If the input array is empty consider it as: [0] (array with a zero).
 */
 
+// Solution A
 function oddOrEven(array) {
-    //enter code here
+    var sum = 0
+    for (let num of array) {
+        sum += num
+    }
+    if (sum % 2 == 0 ) {
+        return 'even'
+    } else {
+        return 'odd'
+    }  
 }
 
-var assert = require('assert');
+// Solution B
+function oddOrEven(array) {
+    return array.reduce((a, b) => a + b, 0) % 2 === 0 ? 'even': 'odd'
+}
 
-describe('oddOrEven', function() {
-    it('Should "odd" or "even" depending on the number', function() {
-        assert.equal(oddOrEven([0]), 'even');
-        assert.equal(oddOrEven([1]), 'odd')
-        assert.equal(oddOrEven([]), 'even')
-    });
-    it('Even tests', function() {
-        assert.equal(oddOrEven([0, 1, 5]), 'even')
-        assert.equal(oddOrEven([0, 1, 3]), 'even')
-        assert.equal(oddOrEven([1023, 1, 2]), 'even')
-    });
-    it('Negative Even tests', function() {
-        assert.equal(oddOrEven([0, -1, -5]), 'even')
-        assert.equal(oddOrEven([0, -1, -3]), 'even')
-        assert.equal(oddOrEven([-1023, 1, -2]), 'even')
-    });
-    it('Odd tests', function() {
-        assert.equal(oddOrEven([0, 1, 2]), 'odd')
-        assert.equal(oddOrEven([0, 1, 4]), 'odd')
-        assert.equal(oddOrEven([1023, 1, 3]), 'odd')
-    });
-    it('Negative Odd tests', function() {
-        assert.equal(oddOrEven([0, -1, 2]), 'odd')
-        assert.equal(oddOrEven([0, 1, -4]), 'odd')
-        assert.equal(oddOrEven([-1023, -1, 3]), 'odd')
-    });
-});
+module.exports = oddOrEven
